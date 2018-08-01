48cda47a298043516cb5c158c0f2a9f3f61611c7
hemasali <hemo3li7@gmail.com>
2018-08-01 15:34:13 +0300

    showing the news in home Page

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,24 +1,57 @@
-const getElementById = (id)=>{
+let mainDiv = document.getElementById("viewsection");
+let submit = document.getElementById("submit");
+const showNews = language => {
+  let loading = document.getElementById("loading");
 
-  document.getElementById(id)
-}
-let submit =  document.getElementById('submit');
-submit.addEventListener('click', () => {
-  let input = document.getElementById('lang');
-  const language = input.value;
-
-  xhrRequest('/get', language, (err, result) => {
+  xhrRequest("/get", language, (err, result) => {
     if (err) {
-      console.log(err.message);
+      loading.style.display = "none";
+
+      let error = document.createElement("h1");
+      error.textContent = err.message;
+      mainDiv.appendChild(error);
     } else {
-      result.articles.forEach((element) => {
-        news = document.createElement('div');
-        news.textContent = element.title;
-        document.getElementById('news').appendChild(news);
-        news.addEventListener('click', () => {
-          console.log(element.description);
-        });
+      loading.style.display = "none";
+      result.articles.forEach(element => {
+        createNews(element);
       });
     }
   });
+};
+showNews("us");
+submit.addEventListener("click", () => {
+  loading.style.display = "block";
+
+  let input = document.getElementById("lang");
+
+  const language = input.value;
+  clearDiv(mainDiv);
+  showNews(language);
 });
+
+const createNews = news => {
+  let article = document.createElement("article");
+  article.classList = "view-item";
+  let img = document.createElement("img");
+  img.setAttribute("src", news.urlToImage);
+  let hr = document.createElement("hr");
+  let title = document.createElement("h3");
+  title.textContent = news.title;
+  let viewMore = document.createElement("a");
+  viewMore.textContent = "Read More";
+  viewMore.addEventListener("click", () => {
+    readMore(news);
+  });
+  article.appendChild(img);
+  article.appendChild(title);
+  article.appendChild(viewMore);
+  mainDiv.appendChild(article);
+  mainDiv.appendChild(hr);
+};
+const readMore = news => {
+  console.log(news.description);
+};
+
+const clearDiv = div => {
+  div.textContent = "";
+};
